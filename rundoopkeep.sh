774d0066618de0265db9c523f004098e48fc2cdd
Christos Vrachas <cvracdi@gmail.com>
2017-09-28 16:14:36 +0300

    script and wallet reflection rules

---
+++ rundoopkeep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for entry in `ls proguard-experiments/apks`; 
+do
+    #execute doop
+
+    #move keep rules of the specific apk to a temp dir
+
+    #rm last run cache
+    echo "$entry"
+done
